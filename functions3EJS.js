09fe3661fd19c6b9974db883f3df40436e998c7b
Roslynn Tellvik <rtellvik@gmail.com>
2016-02-10 09:45:15 -0800

    Functions defined by variable assignment

---
+++ functions3EJS.js
@@ -0,0 +1,79 @@
+/* Function definition.
+Value assigned to variable is a function.
+Function takes one param, x, a number.
+Function returns a number (the square of x).
+*/
+
+// var square = function (x) {
+//   return x*x;
+// };
+// console.log(square(3));
+
+
+/* Function definition.
+Value assigned to variable is a function.
+Function takes no param.
+Function returns nothing. It has a side effect.
+*/
+
+// var makeNoise = function() {
+//   console.log('Pling!');
+// };
+
+// makeNoise();
+
+
+/* Function definition.
+Value assigned to variable is a function.
+Function takes two param, base and exponent, both numbers.
+Function body contains a for loop.
+It returns the value of the base multiplied by the power of the exponent.
+*/
+
+// var power = function(base, exponent) {
+//   var result = 1;
+//   for (var count = 0; count < exponent; count++)
+//     result *= base;
+//   return result;
+// };
+
+// console.log(power(2, 3));
+
+// power(2, 3)
+//   var result = 1;
+// // loop 1:   
+//   for (var count = 0; 0 < 3; 0++) {
+//     1*2; 
+//     return 2;
+//     count = 1;
+// // loop 2: 
+//   for count = 1; 1 < 3; 1++)
+//     2*2;
+//     return 4;
+//     count = 2;
+// // loop 3: 
+//   for count = 2; 2 < 3; 2++)
+//     4*2;
+//     return 8;
+//     count = 3;
+// // loop 4 (condition count < exponent tests false, loop breaks): 
+//   for count = 3; 3 < 3; 3++)
+//     BREAK
+//   return 8;
+// }
+
+
+/* Function definition.
+Value assigned to variable is a function.
+Function takes two param, base and exponent, both numbers.
+Function body contains a for loop
+*/
+
+
+
+
+
+
+
+
+
