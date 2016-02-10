e87b6f91302301f6fed2595323f5b4213dc2033a
Roslynn Tellvik <rtellvik@gmail.com>
2016-02-10 10:13:41 -0800

    added scope examples

--- functions3EJS.js
+++ functions3EJS.js
@@ -63,10 +63,79 @@ It returns the value of the base multiplied by the power of the exponent.
 // }
 
 
-/* Function definition.
-Value assigned to variable is a function.
-Function takes two param, base and exponent, both numbers.
-Function body contains a for loop
+/* Scope as it pertains to arguments and other variables 
+referred to in a given function:
+Arguments passed to a function in the invocation statement
+are local to the function.
+Variables declared within the function (i.e., with the var keyword),
+are local to the function.
+Variables referenced, but not declared, within the function
+will be accessed in the function's enclosing scope(s), up to and
+including the global scope.
+*/
+
+// var x = 'outside';
+
+// var f1 = function() {
+//   var x = 'inside f1';
+// };
+// f1();
+// console.log(x);
+
+/* In the previous example, although f1 has run, it assigns the 
+value 'inside f1' to x only within it's local scope. The console.log
+invocation accesses the value of x in the global scope, 'outside',
+and logs it to console.
+*/
+
+
+// var f2 = function() {
+//   x = 'inside f2';
+// };
+// f2();
+// console.log(x);
+
+/* In the previous example, when f2 is called, control looks for a
+variable declaration for x. It doesn't find one in the local scope of
+the function body. It looks to the enclosing scope (which is the global
+scope in this case) and finds x declared on line 77. It assigns the 
+value 'inside f2' to x within the global scope. The console.log invocation
+on line 96 accesses the value of x in the global scope, which has been changed
+from it's original assignment of 'outside' to 'inside f2' as a result
+of the function invocation on line 95, and logs it to console.
+*/
+
+
+/* NESTED SCOPE */
+// var landscape = function() {
+//   var result = '';
+//   var flat = function(size) {
+//     for (var count = 0; count < size; count++)
+//       result += '_';
+//   };
+//   var mountain = function(size) {
+//     result += '/';
+//     for (var count = 0; count < size; count++)
+//       result += "'";
+//     result += '\\';
+//   };
+  
+//   flat (3);
+//   mountain(4);
+//   flat(6);
+//   mountain(1);
+//   flat(1);
+//   return result;
+// };
+
+// console.log(landscape());
+
+/* In the previous code example, the variable result is accessible
+to both nested functions, flat and mountain. However, the
+count variable is assigned locally within each nested function.
+As a result, when flat and mountain are invoked on lines 123-127,
+each function runs with a fresh count and concatenates the appropriate
+number of symbols to the string, result.
 */
 
 
